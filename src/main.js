02596852de4d516522b2c266673359ce5ecb3f99
enoshima7 <duanjipeng0328@gmail.com>
2020-01-25 21:24:53 +0800

    完成了界面+登录+注册

--- src/main.js
+++ src/main.js
@@ -1,10 +1,26 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import jQuery from 'jquery'
 
+import 'bootstrap'
+import './assets/css/app.scss'
+import 'popper.js'
+import { fb } from './firebase'
+
+window.$ = window.jQuery = jQuery
+
+Vue.component('Navbar', require('./components/NavBar.vue').default)
+
+
+let app = ''
+fb.auth().onAuthStateChanged(function (user) {
+  if (!app) {
+    new Vue({
+      router,
+      render: h => h(App)
+    }).$mount('#app')
+  }
+});
 Vue.config.productionTip = false
 
-new Vue({
-  router,
-  render: h => h(App)
-}).$mount('#app')
--- src/router/index.js
+++ src/router/index.js
@@ -1,27 +1,48 @@
+/* eslint-disable */
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
-
+import Home from '../components/Home.vue'
+import Admin from '../components/Admin.vue'
+import Overview from '../components/Overview'
+import Order from '../components/Order'
+import Product from '../components/Product'
+import Profile from '../components/Profile'
+import { fb } from '../firebase'
 Vue.use(VueRouter)
 
 const routes = [
+  { path: '/', component: Home, name: 'home' },
   {
-    path: '/',
-    name: 'home',
-    component: Home
+    path: '/admin', component: Admin, name: 'admin',
+    meta: { requiresAuth: true },
+    children: [
+      { path: 'overview', component: Overview, name: 'overview' },
+      { path: 'order', component: Order, name: 'order' },
+      { path: 'product', component: Product, name: 'product' },
+      { path: 'profile', component: Profile, name: 'profile' }
+    ]
   },
-  {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+
 ]
 
 const router = new VueRouter({
+  mode: 'history',
+  base: process.env.BASE_URL,
   routes
+})
+
+router.beforeEach((to, from, next) => {
+
+  const requiresAuth = to.matched.some(x => x.meta.requiresAuth)
+  const currentUser = fb.auth().currentUser
+
+  if (requiresAuth && !currentUser) {
+    next('/')
+  } else if (requiresAuth && currentUser) {
+    next()
+  } else {
+    next()
+  }
 })
 
 export default router
